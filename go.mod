053371bc3b4d8bd33ebce1539e6755b13e72995c
David Chapman <talk2dc@gmail.com>
2024-09-09 10:14:18 -0400

    Use go.mod

---
+++ go.mod
@@ -0,0 +1,10 @@
+module github.com/d3chapma/gomarkpdf
+
+go 1.22.5
+
+require (
+	github.com/gomarkdown/markdown v0.0.0-20240730141124-034f12af3bf6
+	github.com/thatisuday/commando v1.0.4
+)
+
+require github.com/thatisuday/clapper v1.0.10 // indirect
--- main.go
+++ main.go
@@ -1,157 +1,160 @@
 package main
 
 import (
-  "fmt"
-  "io"
-  "io/ioutil"
-  "log"
-  "os"
-  "os/exec"
-  "strings"
+	"fmt"
+	"io"
+	"log"
+	"os"
+	"os/exec"
+	"strings"
 
-  "github.com/gomarkdown/markdown"
-  "github.com/gomarkdown/markdown/html"
-  "github.com/thatisuday/commando"
+	"github.com/gomarkdown/markdown"
+	"github.com/gomarkdown/markdown/html"
+	"github.com/thatisuday/commando"
 )
 
 func execute(command string) string {
-  out, err := exec.Command("bash", "-c", command).Output()
+	out, err := exec.Command("bash", "-c", command).Output()
 
-  if err != nil {
-    fmt.Println("%s", err)
-  }
+	if err != nil {
+		fmt.Printf("%s\n", err)
+	}
 
-  output := string(out[:])
-  return output
+	output := string(out[:])
+	return output
 }
 
-func main(){
-  commando.
-  SetExecutableName("markpdf").
-  SetVersion("v1.0.0").
-  SetDescription("A CLI tool to transform markdown files into PDF files")
+func main() {
+	commando.
+		SetExecutableName("markpdf").
+		SetVersion("v1.0.0").
+		SetDescription("A CLI tool to transform markdown files into PDF files")
 
-  commando.
-  Register(nil).
-  AddArgument("file", "Markdown File", "none").
-  AddFlag("theme,t", "Set the theme (colors) for the PDF file", commando.String, "default").
-  AddFlag("orientation,o", "Pass the orientation for the PDF file", commando.String, "Portrait").
-  AddFlag("size,s", "Change page size of PDF file", commando.String, "a4").
-  AddFlag("grayscale,g", "Set grayscale of PDF file", commando.Bool, false).
-  AddFlag("path,p", "Set the final PDF file path", commando.String, "default").
-  AddFlag("css,c", "CSS append files", commando.String, "default").
-  SetAction(markdowntoHTML)
+	commando.
+		Register(nil).
+		AddArgument("file", "Markdown File", "none").
+		AddFlag("theme,t", "Set the theme (colors) for the PDF file", commando.String, "default").
+		AddFlag("orientation,o", "Pass the orientation for the PDF file", commando.String, "Portrait").
+		AddFlag("size,s", "Change page size of PDF file", commando.String, "a4").
+		AddFlag("grayscale,g", "Set grayscale of PDF file", commando.Bool, false).
+		AddFlag("path,p", "Set the final PDF file path", commando.String, "default").
+		AddFlag("css,c", "CSS append files", commando.String, "default").
+		SetAction(markdowntoHTML)
 
-  commando.Parse(nil)
+	commando.Parse(nil)
 }
 
-func markdowntoHTML(args map[string]commando.ArgValue, flags map[string]commando.FlagValue){
+func markdowntoHTML(args map[string]commando.ArgValue, flags map[string]commando.FlagValue) {
 
-  userHome, err := os.UserHomeDir()
-  if err != nil{
-    log.Fatal(err)
-  }
-  packageDirectory := userHome + "/go/src/github.com/edersonferreira/gomarkpdf/"
-  defaultThemes := []string{"default", "dark", "abnt", "programmer"}
-  cmdFlags := ""
-  orientations := []string{"portrait", "landscape"}
-  sizes := []string{"a0","a1","a2","a3","a4","a5","a6","a7","a8","a9","letter"}
+	fmt.Println("MarkdowntoHTML")
+	userHome, err := os.UserHomeDir()
+	if err != nil {
+		log.Fatal(err)
+	}
+	packageDirectory := userHome + "/go/src/github.com/edersonferreira/gomarkpdf/"
+	defaultThemes := []string{"default", "dark", "abnt", "programmer"}
+	cmdFlags := ""
+	orientations := []string{"portrait", "landscape"}
+	sizes := []string{"a0", "a1", "a2", "a3", "a4", "a5", "a6", "a7", "a8", "a9", "letter"}
 
-  markdownFile := args["file"].Value
-  themeBrute, _ := flags["theme"].GetString()
-  orientation, _ := flags["orientation"].GetString()
-  size, _ := flags["size"].GetString()
-  grayscale, _ := flags["grayscale"].GetBool()
-  path, _ := flags["path"].GetString()
-  cssFiles, _ :=   flags["css"].GetString()
+	markdownFile := args["file"].Value
+	themeBrute, _ := flags["theme"].GetString()
+	orientation, _ := flags["orientation"].GetString()
+	size, _ := flags["size"].GetString()
+	grayscale, _ := flags["grayscale"].GetBool()
+	path, _ := flags["path"].GetString()
+	cssFiles, _ := flags["css"].GetString()
 
-  if !Contains(orientations, strings.ToLower(orientation)){
-    fmt.Println("The orientation is with an incorrect value (", orientation,") orientation can be only \"Portrait\" or \"Landscape\"")
-    os.Exit(0)
-  }
-  if !Contains(sizes, strings.ToLower(size)){
-    fmt.Println("The size is with an incorrect value (" + size + "). Size can be only A0 to A9 or Letter")
-    os.Exit(0)
-  }
+	if !Contains(orientations, strings.ToLower(orientation)) {
+		fmt.Println("The orientation is with an incorrect value (", orientation, ") orientation can be only \"Portrait\" or \"Landscape\"")
+		os.Exit(0)
+	}
+	if !Contains(sizes, strings.ToLower(size)) {
+		fmt.Println("The size is with an incorrect value (" + size + "). Size can be only A0 to A9 or Letter")
+		os.Exit(0)
+	}
 
-  if grayscale != false{
-    cmdFlags += "-g "
-  }
+	if grayscale != false {
+		cmdFlags += "-g "
+	}
 
-  cssLinks := ""
+	cssLinks := ""
 
-  if (cssFiles != "default"){
-    cssFilesSplit := strings.Split(cssFiles, ",")
-    for _, css := range cssFilesSplit{
-      cssLinks += "<link rel=\"stylesheet\" href=\"" + css + "\">\n"
-    }
-  }
+	if cssFiles != "default" {
+		cssFilesSplit := strings.Split(cssFiles, ",")
+		for _, css := range cssFilesSplit {
+			cssLinks += "<link rel=\"stylesheet\" href=\"" + css + "\">\n"
+		}
+	}
 
-  cmdFlags += "-O " + orientation + " "
-  cmdFlags += "-s " + size + " "
+	cmdFlags += "-O " + orientation + " "
+	cmdFlags += "-s " + size + " "
 
-  theme := themeBrute
+	theme := themeBrute
 
+	if Contains(defaultThemes, themeBrute) {
+		theme = packageDirectory + "/styles/" + strings.ReplaceAll(themeBrute, ".css", "")
 
-  if Contains(defaultThemes, themeBrute){
-    theme = packageDirectory + "/styles/" + strings.ReplaceAll(themeBrute, ".css", "")
+	}
 
-  }
+	fmt.Println("Reading File")
+	markdownText, err := os.ReadFile(markdownFile)
+	if err != nil {
+		fmt.Println("File reading error", err)
+		return
+	}
 
-  markdownText, err := ioutil.ReadFile(markdownFile)
-  if err != nil {
-    fmt.Println("File reading error", err)
-    return
-  }	
+	flagsHTML := html.CommonFlags | html.CompletePage | html.HrefTargetBlank
+	opts := html.RendererOptions{
+		Flags: flagsHTML,
+	}
+	renderer := html.NewRenderer(opts)
 
-  flagsHTML := html.CommonFlags | html.CompletePage | html.HrefTargetBlank
-  opts := html.RendererOptions{
-    Flags: flagsHTML,
-  }
-  renderer := html.NewRenderer(opts)
+	htmlBody := string(markdown.ToHTML(markdownText, nil, renderer))
+	// não mexe
+	html := "<!DOCTYPE html>\n<html lang=en>\n<meta charset=UTF-8>\n<meta content=\"width=device-width,initial-scale=1\"name=viewport>\n  <link rel=\"stylesheet\" href=\"" + theme + ".css\">\n" + cssLinks + "<body>\n" + htmlBody + "<script>\"h\"==document.body.innerHTML[144]&&\"1\"==document.body.innerHTML[145]&&(document.body.style.marginTop=\"-10010px\")</script>"
 
-  htmlBody := string(markdown.ToHTML(markdownText, nil, renderer))
-  // não mexe
-  html := "<!DOCTYPE html>\n<html lang=en>\n<meta charset=UTF-8>\n<meta content=\"width=device-width,initial-scale=1\"name=viewport>\n  <link rel=\"stylesheet\" href=\"" + theme + ".css\">\n" + cssLinks + "<body>\n" + htmlBody + "<script>\"h\"==document.body.innerHTML[144]&&\"1\"==document.body.innerHTML[145]&&(document.body.style.marginTop=\"-10010px\")</script>"
+	HTMLFile := strings.ReplaceAll(string(markdownFile), "md", "html")
 
-  HTMLFile := strings.ReplaceAll(string(markdownFile), "md", "html")
+	PDFFile := strings.ReplaceAll(string(markdownFile), "md", "pdf")
 
-  PDFFile := strings.ReplaceAll(string(markdownFile), "md", "pdf")
+	if path != "default" {
+		PDFFile = path
+	}
 
-  if path != "default"{
-    PDFFile = path
-  }
+	fmt.Println("Writing HTML file:" + html)
+	WriteToFile(HTMLFile, html)
 
-  WriteToFile(HTMLFile, html)
-
-  generatePDF(HTMLFile, PDFFile, cmdFlags)
+	generatePDF(HTMLFile, PDFFile, cmdFlags)
 }
 
-func generatePDF(file string, PDFFile string, cmdFlags string ){
-  execute("wkhtmltopdf -L 0 -R 0 -B 0 -T 0 " + cmdFlags + "--enable-local-file-access " + file + " " + PDFFile)
-
-  os.Remove(file)
-  fmt.Println("Done")
+func generatePDF(file string, PDFFile string, cmdFlags string) {
+	fmt.Println("Running wkhtmltopdf")
+	fmt.Println(cmdFlags)
+	execute("wkhtmltopdf -L 0 -R 0 -B 0 -T 0 " + cmdFlags + "--enable-local-file-access " + file + " " + PDFFile)
+	fmt.Println("result")
+	// os.Remove(file)
+	fmt.Println("Done")
 }
 
 func WriteToFile(filename string, data string) error {
-  file, err := os.Create(filename)
-  if err != nil {
-    return err
-  }
-  defer file.Close()
+	file, err := os.Create(filename)
+	if err != nil {
+		return err
+	}
+	defer file.Close()
 
-  _, err = io.WriteString(file, data)
-  if err != nil {
-    return err
-  }
-  return file.Sync()
+	_, err = io.WriteString(file, data)
+	if err != nil {
+		return err
+	}
+	return file.Sync()
 }
 func Contains(a []string, x string) bool {
-  for _, n := range a {
-    if x == n {
-      return true
-    }
-  }
-  return false
+	for _, n := range a {
+		if x == n {
+			return true
+		}
+	}
+	return false
 }
